e822c55c59c17770a5d256e2b06c187559a808cb
yefcion <yefcion@163.com>
2020-04-23 22:13:02 +0800

    bat

---
+++ 锁屏照片在这里.bat
@@ -0,0 +1,11 @@
+:: 首先到C盘 
+:: C:\Users（用户）\Administrator（当前登录用户名）\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_********\LocalState\Assets
+:: 找到文件位置，把此脚本放到你那里
+
+md D:\0_QingQ_look_at_me_love_love_hihihihihihihi
+
+copy * D:\0_QingQ_look_at_me_love_love_hihihihihihihi
+
+cd /d D:\0_QingQ_look_at_me_love_love_hihihihihihihi
+
+Ren * *.png
